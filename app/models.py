333df0fed8d8ad263e06864fc9be602789671208
abhishek-kbizsoft <abhishekkumarphp.kbizsoft@gmail.com>
2025-01-01 19:52:03 +0530

    admin dashboard and ticket status system

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(150), nullable=False)  # Username field
     email = db.Column(db.String(120), unique=True, nullable=False)  # Email field
     password = db.Column(db.String(200), nullable=False)  # Password field
+    role = db.Column(db.Integer, default=1) # 0 for admin, 1 for normal user
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -16,6 +17,7 @@ class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.Integer, default=1) # 0 for success, 1 for pending, 2 for reject
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Foreign Key to User
 
     user = db.relationship('User', backref=db.backref('tickets', lazy=True))
